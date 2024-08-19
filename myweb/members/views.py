659a577bf219ae552be12bf74f736f0dc0ffacf6
ZaneL777 <6629011012@cdti.ac.th>
2024-08-19 16:36:39 +0700

    first commit

---
+++ myweb/members/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+
+
+
+def profile(request):
+    template = loader.get_template('profile.html')
+    return HttpResponse(template.render())
+
+
+def blogs(request):
+    template = loader.get_template('blogs.html')
+    return HttpResponse(template.render())
